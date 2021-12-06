3f04183b6eed67160124158af5e1ebd91d6d36bf
Irina-Koida <91370760+Irina-Koida@users.noreply.github.com>
2021-12-06 02:22:07 +0200

    // added simple tests ( tap a button and navigate to the page. It works! )

---
+++ MyCalendarApp/POM/FossilButton.cs
@@ -0,0 +1,76 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace MyCalendarApp.POM
+{
+    class FossilButton
+    {
+        private readonly AppiumDriver<AndroidElement> _driver;
+
+        public FossilButton(AppiumDriver<AndroidElement> appiumDriver)
+        {
+            _driver = appiumDriver;
+        }
+
+        private readonly By _mapButton = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+            "android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/" +
+            "android.view.View/android.view.View/android.view.View[11]/android.view.View/android.widget.ListView/" +
+            "android.view.View[1]/android.view.View");
+        private readonly By _hubButton = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+            "android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/" +
+            "android.view.View/android.view.View[11]/android.view.View/android.widget.ListView/android.view.View[2]/" +
+            "android.view.View");
+        private readonly By _helpButton = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+            "android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/" +
+            "android.view.View/android.view.View[11]/android.view.View/android.widget.ListView/android.view.View[3]/" +
+            "android.view.View");
+        private readonly By _moreButton = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+            "android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/" +
+            "android.view.View/android.view.View[11]/android.view.View/android.widget.ListView/android.view.View[4]/" +
+            "android.view.View");
+        private readonly By _findFossilButton = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+           "android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/" +
+           "android.view.View/android.view.View[10]");
+
+        private readonly By _mapPage = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+            "android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/" +
+            "android.view.View/android.view.View[1]/android.view.View[2]");
+        private readonly By _helpPage = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+            "android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/" +
+            "android.view.View/android.view.View[1]/android.view.View[2]");
+        private readonly By _hubPage = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+            "android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/" +
+            "android.view.View/android.view.View[3]");
+        private readonly By _contentPage = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
+            "android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/" +
+            "android.view.View/android.view.View[1]/android.view.View[2]");
+
+        public void TapFindFossil(string findFossilButton) =>
+         _driver.FindElement(_findFossilButton).Click();
+
+        public void TapMapButton(string mapButton) =>
+        _driver.FindElement(_mapButton).Click();
+
+        public void TapHubButton(string hubButton) =>
+       _driver.FindElement(_hubButton).Click();
+
+        public void TapHelpButton(string helpButton) =>
+       _driver.FindElement(_helpButton).Click();
+
+        public void TapMoreButton(string moreButton) =>
+       _driver.FindElement(_moreButton).Click();
+
+        public string MapPage() =>
+            _driver.FindElement(_mapPage).Text;
+
+        public string HubPage() =>
+        _driver.FindElement(_hubPage).Text;
+
+        public string HelpPage() =>
+         _driver.FindElement(_helpPage).Text;
+
+        public string ContentPage() =>
+         _driver.FindElement(_contentPage).Text;
+    }
+}
---
+++ MyCalendarApp/Steps/FossilTapAButtonSteps.cs
@@ -0,0 +1,92 @@
+﻿using MyCalendarApp.POM;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.Android;
+using TechTalk.SpecFlow;
+
+namespace MyCalendarApp.Steps
+{
+    [Binding]
+    public class FossilTapAButtonSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly AndroidDriver<AndroidElement> _androidDriver;
+        private readonly FossilButton _fossilButton;
+
+        public FossilTapAButtonSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+            _androidDriver = _scenarioContext.Get<AndroidDriver<AndroidElement>>("driver");
+            _fossilButton = new FossilButton(_androidDriver);
+        }
+
+        [When(@"I tap a ""(.*)"" button")]
+        public void WhenITapAButton(string findFossilButton)
+        {
+            _fossilButton.TapFindFossil(findFossilButton);
+        }
+
+        [Then(@"I navigate to the map, to find a fossil")]
+        public void ThenINavigateToTheMapToFindAFossil()
+        {
+            string mapPage = _fossilButton.MapPage();
+            bool result = mapPage.Contains(mapPage);
+            Assert.AreEqual(actual: result, expected: true);
+        }
+
+        [When(@"I tap ""(.*)"" button")]
+        public void WhenITapButton(string mapButton)
+        {
+            _fossilButton.TapMapButton(mapButton);
+        }
+
+        [Then(@"I navigate to the map")]
+        public void ThenINavigateToTheMap()
+        {
+            string mapPage = _fossilButton.MapPage();
+            bool result = mapPage.Contains(mapPage);
+            Assert.AreEqual(actual: result, expected: true);
+        }
+
+        [When(@"I tap the ""(.*)"" button")]
+        public void WhenITapTheButton(string hubButton)
+        {
+            _fossilButton.TapHubButton(hubButton);
+        }
+
+        [Then(@"I navigate to the hub page")]
+        public void ThenINavigateToTheHubPage()
+        {
+            string hubPage = _fossilButton.HubPage();
+            bool result = hubPage.Contains(hubPage);
+            Assert.AreEqual(actual: result, expected: true);
+        }
+
+        [When(@"I tap in ""(.*)"" button")]
+        public void WhenITapInButton(string helpButton)
+        {
+            _fossilButton.TapHelpButton(helpButton);
+        }
+
+        [Then(@"I navigate to the help page, where it is written how to use this application")]
+        public void ThenINavigateToTheHelpPageWhereItIsWrittenHowToUseThisApplication()
+        {
+            string helpPage = _fossilButton.HelpPage();
+            bool result = helpPage.Contains(helpPage);
+            Assert.AreEqual(actual: result, expected: true);
+        }
+
+        [When(@"Tap the ""(.*)"" button")]
+        public void WhenTapTheButton(string moreButton)
+        {
+            _fossilButton.TapMoreButton(moreButton);
+        }
+
+        [Then(@"Navigate to the content page")]
+        public void ThenNavigateToTheContentPage()
+        {
+            string contentPage = _fossilButton.ContentPage();
+            bool result = contentPage.Contains(contentPage);
+            Assert.AreEqual(actual: result, expected: true);
+        }
+    }
+}
